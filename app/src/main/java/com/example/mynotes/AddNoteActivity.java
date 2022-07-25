38551651db642a73d8230cf4b3b85a50bb4ce01a
AnnyVJu <vladykina.anna.ju@gmail.com>
2022-07-25 16:16:58 +0300

    Add note UI interface

--- app/src/main/java/com/example/mynotes/AddNoteActivity.java
+++ app/src/main/java/com/example/mynotes/AddNoteActivity.java
@@ -3,6 +3,7 @@ package com.example.mynotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class AddNoteActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class AddNoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+    }
+
+    public void onClickSaveNote(View view) {
     }
 }
