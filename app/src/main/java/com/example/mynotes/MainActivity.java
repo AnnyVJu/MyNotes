3313ecdcf07b059adad6882ed48e9bbe7eacbbd5
AnnyVJu <vladykina.anna.ju@gmail.com>
2022-07-26 12:10:27 +0300

    Remove item on swipe

--- app/src/main/java/com/example/mynotes/MainActivity.java
+++ app/src/main/java/com/example/mynotes/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.mynotes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -15,12 +18,13 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerViewNotes;
     public static final ArrayList<Note> notes = new ArrayList<>();
+    private NotesAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerViewNotes=findViewById(R.id.recyclerViewNotes);
+        recyclerViewNotes = findViewById(R.id.recyclerViewNotes);
         if (notes.isEmpty()) {
             notes.add(new Note("Ортодонт", "Запись к стоматологу", "Вторник", 1));
             notes.add(new Note("Врач", "Запись в поликлинику", "Вторник", 1));
@@ -28,11 +32,40 @@ public class MainActivity extends AppCompatActivity {
             notes.add(new Note("Магазин", "Купить крепеж", "Суббота", 2));
             notes.add(new Note("Дом", "Прибраться в шкафу", "Воскресенье", 3));
             notes.add(new Note("Дом", "Помыть окна", "Вторник", 3));
-            NotesAdapter adapter = new NotesAdapter(notes);
-            recyclerViewNotes.setLayoutManager(new LinearLayoutManager(this));
-            recyclerViewNotes.setAdapter(adapter);
         }
+        adapter = new NotesAdapter(notes);
+        recyclerViewNotes.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewNotes.setAdapter(adapter);
+        adapter.setOnNoteClickListener(new NotesAdapter.OnNoteClickListener() {
+            @Override
+            public void onNoteClick(int position) {
+                Toast.makeText(MainActivity.this, "clicked", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLongClick(int position) {
+                remove(position);
+            }
+        });
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                remove(viewHolder.getAdapterPosition());
+            }
+        });
+        itemTouchHelper.attachToRecyclerView(recyclerViewNotes);
     }
+
+    private void remove(int position) {
+        notes.remove(position);
+        adapter.notifyDataSetChanged();
+    }
+
 
     public void onClickAddNote(View view) {
         Intent intent = new Intent(this, AddNoteActivity.class);
