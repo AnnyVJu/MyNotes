5df9d088f5ce040ad490d763f0db4fbe6da9a159
AnnyVJu <vladykina.anna.ju@gmail.com>
2022-08-03 12:44:16 +0300

    Add new database

--- app/src/main/java/com/example/mynotes/AddNoteActivity.java
+++ app/src/main/java/com/example/mynotes/AddNoteActivity.java
@@ -2,13 +2,16 @@ package com.example.mynotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class AddNoteActivity extends AppCompatActivity {
 
@@ -17,10 +20,15 @@ public class AddNoteActivity extends AppCompatActivity {
     private Spinner spinnerDaysOfWeek;
     private RadioGroup radioGroupPriority;
 
+    private NotesDBHelper dbHelper;
+    private SQLiteDatabase database;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+        dbHelper = new NotesDBHelper(this);
+        database = dbHelper.getWritableDatabase();
         editTextTitle = findViewById(R.id.editTextTitle);
         editTextDescription = findViewById(R.id.editTextDescription);
         spinnerDaysOfWeek = findViewById(R.id.spinnerDaysOfWeek);
@@ -34,8 +42,20 @@ public class AddNoteActivity extends AppCompatActivity {
         int radioButtonId = radioGroupPriority.getCheckedRadioButtonId();
         RadioButton radiobutton = findViewById(radioButtonId);
         int priority = Integer.parseInt(radiobutton.getText().toString());
-        Note note = new Note(title, description, dayOfWeek, priority);
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        if(isFilled(title, description)) {
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(NotesContract.NotesEntry.COLUMN_TITLE, title);
+            contentValues.put(NotesContract.NotesEntry.COLUMN_DESCRIPTION, description);
+            contentValues.put(NotesContract.NotesEntry.COLUMN_DAY_OF_WEEK, dayOfWeek);
+            contentValues.put(NotesContract.NotesEntry.COLUMN_PRIORITY, priority);
+            database.insert(NotesContract.NotesEntry.TABLE_NAME, null, contentValues);
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, R.string.warning_fill_fields, Toast.LENGTH_SHORT).show();
+        }
+    }
+    private boolean isFilled(String title, String description) {
+        return !title.isEmpty() && !description.isEmpty();
     }
 }
